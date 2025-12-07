2ee4eaa65b615dba109bfd34141ae181e45ac443
MossDog <lukejohnmosshughes@gmail.com>
2025-12-07 16:57:19 +0000

    first commit

---
+++ Day1/solution.py
@@ -0,0 +1,12 @@
+instructions = [int(line.strip().replace("L", "-").replace("R", "")) for line in open("input.txt")]
+
+dial_pos = 50
+mod = 100
+count = 0
+
+for instruction in instructions:
+    dial_pos = (dial_pos + instruction) % mod
+    if dial_pos == 0:
+        count += 1
+
+print(count)
